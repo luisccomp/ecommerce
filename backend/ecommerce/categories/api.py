4a8af4c479f2e7d7aadd86d82854c487932048e6
luisccomp <luisccomp@gmail.com>
2021-03-16 12:05:56 -0300

    Updated categories api

--- backend/ecommerce/categories/api.py
+++ backend/ecommerce/categories/api.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from flask import Blueprint, request, jsonify
+from flask_jwt import jwt_required
 from marshmallow.exceptions import ValidationError
 from .models import Category
 from .serializers import category_schema, category_schemas, category_response_schema
@@ -9,6 +10,7 @@ blueprint = Blueprint('categories', __name__)
 
 
 @blueprint.route('/api/categories', methods=('POST',))
+@jwt_required()
 def create_category():
     try:
         category_create_request = category_schema.load(data=request.json)
@@ -40,6 +42,7 @@ def find_category_by_id(category_id):
 
 
 @blueprint.route('/api/categories/<int:category_id>', methods=('PUT',))
+@jwt_required()
 def update_category(category_id):
     try:
         category_update_request = category_schema.load(data=request.json)
@@ -57,6 +60,7 @@ def update_category(category_id):
 
 
 @blueprint.route('/api/categories/<int:category_id>', methods=('DELETE',))
+@jwt_required()
 def delete_category(category_id):
     category = Category.query.get(category_id)
 
